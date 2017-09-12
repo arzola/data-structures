456ef40358e151648028a9191f4c5eaa256cc79e
Oscar Arzola <oscararzola@gmail.com>
2017-09-12 05:58:18 -0500

    LinkedList

---
+++ src/mx/icode/LinkedList.java
@@ -0,0 +1,40 @@
+package mx.icode;
+
+class LinkedList {
+
+    private Node Head;
+
+    class Node {
+
+        private int value;
+        public Node next = null;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void LinkedList(Node head) {
+        this.Head = head;
+    }
+
+    public boolean append(int val) {
+
+        if (this.Head.next == null) {
+            Node next = new Node(val);
+            this.Head.next = next;
+            return true;
+        }
+
+        Node current = this.Head;
+
+        while(current.next!=null){
+            current = current.next.next;
+        }
+
+        current.next = new Node(val);
+        return true;
+
+    }
+
+}
---
+++ src/mx/icode/Main.java
@@ -0,0 +1,8 @@
+package mx.icode;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
